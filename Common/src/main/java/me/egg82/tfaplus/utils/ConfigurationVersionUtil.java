d40a8058b1a0f291b70e304c5427c35dc8dce18c
egg82 <phantom_zero@ymail.com>
2018-11-21 19:16:45 -0700

    Added 2faplus commands to protected Fixed accidental versioning in plugin.yml

--- Common/src/main/java/me/egg82/tfaplus/utils/ConfigurationVersionUtil.java
+++ Common/src/main/java/me/egg82/tfaplus/utils/ConfigurationVersionUtil.java
@@ -1,10 +1,14 @@
 package me.egg82.tfaplus.utils;
 
 import com.google.common.io.Files;
+import com.google.common.reflect.TypeToken;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 
 public class ConfigurationVersionUtil {
     private ConfigurationVersionUtil() {}
@@ -12,10 +16,10 @@ public class ConfigurationVersionUtil {
     public static void conformVersion(ConfigurationLoader<ConfigurationNode> loader, ConfigurationNode config, File fileOnDisk) throws IOException {
         double oldVersion = config.getNode("version").getDouble(1.0d);
 
-        /*if (config.getNode("version").getDouble(1.0d) == 1.0d) {
-            to20(config);
+        if (config.getNode("version").getDouble(1.0d) == 1.0d) {
+            to11(config);
         }
-        if (config.getNode("version").getDouble() == 2.0d) {
+        /*if (config.getNode("version").getDouble() == 2.0d) {
             to30(config);
         }*/
 
@@ -28,5 +32,31 @@ public class ConfigurationVersionUtil {
             Files.copy(fileOnDisk, backupFile);
             loader.save(config);
         }
+    }
+
+    private static void to11(ConfigurationNode config) {
+        // Add 2FAPlus to commands
+        List<String> sources;
+        try {
+            sources = config.getNode("2fa", "command-list").getList(TypeToken.of(String.class));
+        } catch (ObjectMappingException ex) {
+            sources = new ArrayList<>();
+        }
+        if (!sources.contains("2faplus")) {
+            sources.add("2faplus");
+        }
+        if (!sources.contains("tfaplus")) {
+            sources.add("tfaplus");
+        }
+        if (!sources.contains("2fa")) {
+            sources.add("2fa");
+        }
+        if (!sources.contains("tfa")) {
+            sources.add("tfa");
+        }
+        config.getNode("2fa", "command-list").setValue(sources);
+
+        // Version
+        config.getNode("version").setValue(1.1d);
     }
 }
