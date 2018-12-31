66391ac80df1eb7157c926a040c5e852a29d7075
egg82 <phantom_zero@ymail.com>
2018-12-30 20:52:15 -0700

    Fixed issue with maps throwing errors on <= 1.12.2

--- Bukkit/src/main/java/me/egg82/tfaplus/utils/MapUtil.java
+++ Bukkit/src/main/java/me/egg82/tfaplus/utils/MapUtil.java
@@ -1,6 +1,8 @@
 package me.egg82.tfaplus.utils;
 
 import java.awt.image.BufferedImage;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 import me.egg82.tfaplus.renderers.ImageRenderer;
 import me.egg82.tfaplus.renderers.QRRenderer;
@@ -10,6 +12,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.MapMeta;
 import org.bukkit.map.MapView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Largely taken from SecureMyAccount
@@ -17,7 +21,20 @@ import org.bukkit.map.MapView;
  */
 
 public class MapUtil {
+    private static final Logger logger = LoggerFactory.getLogger(MapUtil.class);
+
     private MapUtil() {}
+
+    private static Method getMapId = null;
+
+    static {
+        // Ugly hack, explanation below
+        try {
+            getMapId = MapView.class.getMethod("getId");
+        } catch (NoSuchMethodException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
 
     private static final Material MAP;
     static {
@@ -44,7 +61,7 @@ public class MapUtil {
         if (BukkitVersionUtil.isAtLeast("1.13")) {
             map = new ItemStack(MAP, 1);
         } else {
-            map = new ItemStack(MAP, 1, (short) view.getId());
+            map = new ItemStack(MAP, 1, getMapId(view));
         }
         setData(map, "TOTP code - DESTROY AFTER USE", view);
         ItemStack oldItem;
@@ -75,7 +92,7 @@ public class MapUtil {
         if (BukkitVersionUtil.isAtLeast("1.13")) {
             map = new ItemStack(MAP, 1);
         } else {
-            map = new ItemStack(MAP, 1, (short) view.getId());
+            map = new ItemStack(MAP, 1, getMapId(view));
         }
         setData(map, "HOTP code - DESTROY AFTER USE", view);
         ItemStack oldItem;
@@ -117,5 +134,17 @@ public class MapUtil {
         }
         meta.setDisplayName(name);
         stack.setItemMeta(meta);
+    }
+
+    // This is an ugly hack, but..
+    // Basically compiling against 1.13 expects a returned int, <= 1.12 expects a returned short
+    // So using getId will break when compiling with one vs the other
+    private static short getMapId(MapView view) {
+        try {
+            return (short) getMapId.invoke(view);
+        } catch (IllegalAccessException | InvocationTargetException ex) {
+            logger.error(ex.getMessage(), ex);
+            return (short) -1;
+        }
     }
 }
